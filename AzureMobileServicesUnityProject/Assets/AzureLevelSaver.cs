4327e3c749642d9fe38e25fe22daa894bf4fcbc8
Vaughan Knight <vaughan@bitrave.com>
2013-06-24 03:18:50 +1000

    Project for Azure Mobile Services in Unity

---
+++ AzureMobileServicesUnityProject/Assets/AzureLevelSaver.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections.Generic;
+using Bitrave.Azure;
+using Newtonsoft.Json;
+using UnityEngine;
+using System.Collections;
+
+public class AzureLevelSaver : MonoBehaviour
+{
+
+    void SaveLevel()
+    {
+        var azure = new AzureMobileServices(_azureEndPoint, _applicationKey);
+        var data = new LevelSaveData() { SaveData = SavePositions(), Id = 1 };
+        azure.Update<LevelSaveData>(data);
+    }
+
+    void LoadLevel()
+    {
+        var azure = new AzureMobileServices(_azureEndPoint, _applicationKey);
+        
+        azure.Lookup<LevelSaveData>(1, azureResponse =>
+        {
+            if (azureResponse.Status == AzureResponseStatus.Success)
+            {
+                var data = azureResponse.ResponseData;
+                var cubeData = JsonConvert.DeserializeObject<List<Vector3>>(data.SaveData);
+                for (var i = 0; i < cubeData.Count && i < cubes.Count; i++)
+                {
+                    cubes[i].position = cubeData[i];
+                }
+            }
+        });
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    private Ray ray;
+    private bool dragEnable;
+    private float distance;
+    private GameObject dragObject;
+
+    public void OnGUI()
+    {
+        var screenScale = Screen.width / 820.0f;
+        var scaledMatrix = Matrix4x4.Scale(new Vector3(screenScale, screenScale, screenScale));
+        GUI.matrix = scaledMatrix;
+
+        GUILayout.BeginVertical();
+        GUILayout.MaxWidth(300);
+        GUILayout.Width(300);
+
+
+        SaveLoadCubes();
+
+
+        GUILayout.EndVertical();
+    }
+
+
+
+    string SavePositions()
+    {
+        var list = new List<Vector3>();
+        foreach (var t in cubes)
+        {
+            if (t == null)
+                break;
+            list.Add(t.position);
+        }
+        var s = JsonConvert.SerializeObject(list, Formatting.Indented,
+            new JsonSerializerSettings { ContractResolver = new AzureMobileServicesUI.ShouldSerializeContractResolver() });
+        return s;
+    }
+
+    void LoadPositions()
+    {
+        try
+        {
+            LoadLevel();
+        }
+        catch (System.Exception ex)
+        {
+
+            string s91113 = "[\r\n  {\r\n    \"x\": 1032.202,\r\n    \"y\": 32.82989,\r\n    \"z\": 1314.85547,\r\n    \"magnitude\": 1671.93408,\r\n    \"sqrMagnitude\": 2795363.75\r\n  },\r\n  {\r\n    \"x\": 1032.202,\r\n    \"y\": 32.82989,\r\n    \"z\": 1314.85547,\r\n    \"magnitude\": 1671.93408,\r\n    \"sqrMagnitude\": 2795363.75\r\n  },\r\n  {\r\n    \"x\": 1032.202,\r\n    \"y\": 32.82989,\r\n    \"z\": 1314.85547,\r\n    \"magnitude\": 1671.93408,\r\n    \"sqrMagnitude\": 2795363.75\r\n  },\r\n  {\r\n    \"x\": 1034.47632,\r\n    \"y\": 32.8721848,\r\n    \"z\": 1310.3761,\r\n    \"magnitude\": 1669.82251,\r\n    \"sqrMagnitude\": 2788307.25\r\n  },\r\n  {\r\n    \"x\": 1029.17468,\r\n    \"y\": 35.4239,\r\n    \"z\": 1311.92139,\r\n    \"magnitude\": 1667.81079,\r\n    \"sqrMagnitude\": 2781593.0\r\n  },\r\n  {\r\n    \"x\": 1032.75891,\r\n    \"y\": 35.2984238,\r\n    \"z\": 1310.73193,\r\n    \"magnitude\": 1669.08813,\r\n    \"sqrMagnitude\": 2785855.25\r\n  }\r\n]";
+            var cubeData = JsonConvert.DeserializeObject<List<Vector3>>(s91113);
+            for (var i = 0; i < cubeData.Count && i < cubes.Count; i++)
+            {
+                cubes[i].position = cubeData[i];
+            }
+        }
+    }
+
+    private class LevelSaveData
+    {
+        public int? Id { get; set; }
+        public string SaveData { get; set; }
+    }
+    public List<Transform> cubes;
+    private void SaveLoadCubes()
+    {
+        GUILayout.BeginArea(new Rect(650, 10, 150, 300));
+
+
+        if (GUILayout.Button("Save Layout", GUILayout.Width(150), GUILayout.Height(80)))
+        {
+            SaveLevel();
+        }
+        if (GUILayout.Button("Load Layout", GUILayout.Width(150), GUILayout.Height(80)))
+        {
+            LoadPositions();
+        }
+
+
+        GUILayout.EndArea();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0 || Input.GetMouseButton(0))
+        {
+            TouchPhase phase = TouchPhase.Moved;
+            Vector2 position;
+
+            if (Input.touchCount > 0)
+            {
+                phase = Input.GetTouch(0).phase;
+                position = Input.GetTouch(0).position;
+            }
+            else
+            {
+                position = Input.mousePosition;
+            }
+
+            switch (phase)
+            {
+                case TouchPhase.Canceled:
+                case TouchPhase.Ended:
+                    {
+                        dragEnable = false;
+                        break;
+                    }
+                case TouchPhase.Moved:
+                case TouchPhase.Stationary:
+                case TouchPhase.Began:
+                    {
+                        ray = Camera.main.ScreenPointToRay(position);
+                        RaycastHit hit;
+                        LayerMask mask = 1 << 8;
+
+                        if (!dragEnable && Physics.Raycast(ray, out hit, 1000f, mask))
+                        {
+                            dragEnable = true;
+                            distance = hit.distance;
+                            dragObject = hit.collider.gameObject;
+                        }
+                        if (dragEnable)
+                        {
+                            Ray newRay = Camera.main.ScreenPointToRay(position);
+                            newRay.origin = Camera.main.transform.position;
+                            var pos = newRay.GetPoint(10f);
+                            dragObject.transform.position = pos;
+                        }
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            dragEnable = false;
+        }
+    }
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    private string _azureEndPoint = "http://bitraveservices.azure-mobile.net/";
+    private string _applicationKey = "ePYhutMVmiUPWhFAJaRYTJsPFTiuAB20";
+
+}
---
+++ AzureMobileServicesUnityProject/Assets/AzureMobileServicesUI.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+//using Bitrave.Azure;
+//using Bitrave.LiveApps.Azure;
+//using Newtonsoft.Json;
+//using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using UnityEngine;
+using System.Collections;
+//using AzureMobileServices = Bitrave.Azure.AzureMobileServices;
+
+public class AzureMobileServicesUI : MonoBehaviour {
+
+//    // Use this for initialization
+//    void Start () {
+	
+//    }
+	
+//    // Update is called once per frame
+//    void Update () {
+	
+//    }
+
+//    public class ToDoItem
+//    {
+//        public ToDoItem()
+//        {
+//            Text = "";
+//            Complete = false;
+//        }
+
+//        public int? Id { get; set; }
+
+//        [JsonProperty(PropertyName = "text")]
+//        public string Text { get; set; }
+
+//        [JsonProperty(PropertyName = "complete")]
+//        public bool Complete { get; set; }
+
+//        public override string ToString()
+//        {
+//            return Id + "," + Text + "," + Complete;
+//        }
+//    }
+    public class LevelScore
+    {
+        public int? Id { get; set; }
+        public DateTime? Date { get; set; }
+        public string UserId { get; set; }
+        public string Level { get; set; }
+        public int Bonuses { get; set; }
+        public float Time { get; set; }
+        public bool JellySaved { get; set; }
+
+        public override string ToString()
+        {
+            return UserId;
+        }
+    }
+
+    //private ToDoItem addItem = new ToDoItem(){Text="", Complete = false};
+    //private ToDoItem lookupItem = new ToDoItem(){Text="", Complete = false, Id=1};
+
+    public class ShouldSerializeContractResolver : DefaultContractResolver
+    {
+        public new static readonly ShouldSerializeContractResolver Instance = new ShouldSerializeContractResolver();
+
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            JsonProperty property = base.CreateProperty(member, memberSerialization);
+
+            if (property.DeclaringType == typeof(Vector3) && property.PropertyName == "normalized")
+            {
+                property.ShouldSerialize =
+                  instance =>
+                  {
+                      return false;
+                  };
+            }
+
+            return property;
+        }
+    }
+//}
+//    public void OnGUI()
+//    {
+//        var screenScale = Screen.width / 820.0f;
+//        var scaledMatrix = Matrix4x4.Scale(new Vector3(screenScale, screenScale, screenScale));
+//        GUI.matrix = scaledMatrix;
+
+//        GUILayout.BeginVertical();
+//        GUILayout.MaxWidth(300);
+//        GUILayout.Width(300);
+
+//        AddUI();
+//        LookupUI();
+//        ReadUI();
+//        WhereUI();
+
+//        GUILayout.BeginArea(new Rect(650, 10, 150, 200));
+//        if (GUILayout.Button("Launch Auth", GUILayout.Width(150), GUILayout.Height(50)))
+//        {
+//            var azure = new AzureMobileServices(_azureEndPoint, _applicationKey);
+//            azure.LoginAsync(AuthenticationProvider.Facebook, response =>
+//                {
+//                    if (response.Status == AzureResponseStatus.Success)
+//                    {
+//                        Debug.Log(response.ResponseData.UserId + " - " +
+//                                  response.ResponseData.MobileServiceAuthenticationToken);
+//                    }
+//                });
+//        }
+//        if (GUILayout.Button("Back to Menu", GUILayout.Width(150), GUILayout.Height(50)))
+//        {
+//            Application.LoadLevel("Menu");
+//        }
+//        GUILayout.EndArea();
+
+//        GUILayout.EndVertical();
+//    }
+
+//    private string _azureEndPoint = "https://bitraveservices.azure-mobile.net/";
+//    private string _applicationKey = "ePYhutMVmiUPWhFAJaRYTJsPFTiuAB20";
+
+
+//    private void AddUI()
+//    {
+//        GUILayout.BeginArea(new Rect(10, 10, 150, 200));
+//        GUILayout.Label("Add Todo Item");
+//        GUILayout.Label("Id:" + addItem.Id);
+//        addItem.Text = GUILayout.TextField(addItem.Text);
+        
+//        addItem.Complete = GUILayout.Toggle(addItem.Complete, "Complete");
+//        if (GUILayout.Button("Add Item"))
+//        {
+//            var azure = new AzureMobileServices(_azureEndPoint, _applicationKey);
+//            azure.OnError = response => { Debug.Log(response.Error); };
+//            azure.Insert(addItem);
+//        }
+//        GUILayout.EndArea();
+//    }
+
+//    private void LookupUI()
+//    {
+//        GUILayout.BeginArea(new Rect(170, 10, 150, 250));
+//        GUILayout.Label("Lookup/Delete Todo Item");
+
+//        if (lookupItem == null)
+//            lookupItem = new ToDoItem() { Text = "", Complete = false, Id = 1 };
+
+//        GUILayout.Label("ID");
+//        var idString = lookupItem.Id == null ? "0" : lookupItem.Id.ToString();
+//        int itemId = 0;
+//        if (int.TryParse(GUILayout.TextField(idString), out itemId))
+//        {
+//            lookupItem.Id = itemId;
+//        }
+//        GUILayout.Label("Text");
+//        lookupItem.Text = GUILayout.TextField(lookupItem.Text);
+//        lookupItem.Complete = GUILayout.Toggle(lookupItem.Complete, "Complete");
+
+//        if (GUILayout.Button("Lookup Item"))
+//        {
+//            var azure = new AzureMobileServices(_azureEndPoint, _applicationKey);
+//            azure.OnError = response => { Debug.Log(response.Error); };
+//            azure.Lookup<ToDoItem>(itemId, response =>
+//                {
+//                    lookupItem = response.ResponseData;
+//                });
+//        }
+//        if (GUILayout.Button("Delete Item"))
+//        {
+//            var azure = new AzureMobileServices(_azureEndPoint, _applicationKey);
+//            azure.OnError = response => { Debug.Log(response.Error); };
+//            azure.Delete(lookupItem);
+//            lookupItem = new ToDoItem() {Text="", Complete = false};
+//        }
+//        if (GUILayout.Button("Update Item"))
+//        {
+//            var azure = new AzureMobileServices(_azureEndPoint, _applicationKey);
+//            azure.OnError = response => { Debug.Log(response.Error); };
+//            azure.Update(lookupItem);
+//        }
+//        GUILayout.EndArea();
+//    }
+
+//    private List<ToDoItem> allToDoItems = new List<ToDoItem>();
+//    private Vector2 scrollPosition = Vector2.zero;
+//    private void ReadUI()
+//    {
+//        GUILayout.BeginArea(new Rect(330, 10, 150, 400));
+//        GUILayout.Label("Read Todo Items");
+
+//        if (GUILayout.Button("Lookup Item"))
+//        {
+//            var azure = new AzureMobileServices(_azureEndPoint, _applicationKey);
+//            azure.OnError = response => { Debug.Log(response.Error); };
+//            azure.Read<ToDoItem>(response =>
+//                {
+//                    allToDoItems = response.ResponseData;
+//                });
+//        }
+
+//        scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, true);
+//        foreach (var item in allToDoItems)
+//        {
+//            GUILayout.Label(item.Id + ":" + item.Text);
+//            GUILayout.Toggle(item.Complete, "Complete");
+//        }
+//        GUILayout.EndScrollView();
+
+//        GUILayout.EndArea();
+//    }
+
+//    private List<ToDoItem> whereToDoItems = new List<ToDoItem>();
+//    private Vector2 whereScrollPosition = Vector2.zero;
+//    private ToDoItem whereItem = new ToDoItem(){Text = "", Complete = false};
+//    private void WhereUI()
+//    {
+//        GUILayout.BeginArea(new Rect(490, 10, 150, 400));
+//        GUILayout.Label("Where Todo Items");
+
+//        whereItem.Text = GUILayout.TextField(whereItem.Text);
+//        whereItem.Complete = GUILayout.Toggle(whereItem.Complete, "Complete");
+        
+//        if (GUILayout.Button("Query Items"))
+//        {
+//            var azure = new AzureMobileServices(_azureEndPoint, _applicationKey);
+//            azure.OnError = response => { Debug.Log(response.Error); };
+//            Expression<System.Func<ToDoItem, bool>> myExpression;
+//            if (!String.IsNullOrEmpty(whereItem.Text))
+//            {
+//                myExpression =
+//                x => x.Text.Contains(whereItem.Text) && whereItem.Complete == x.Complete;
+//            }
+//            else
+//            {
+//                myExpression = x => whereItem.Complete == x.Complete;
+//            }
+        
+//            azure.Where<ToDoItem>(myExpression, response =>
+//            {
+//                whereToDoItems = response.ResponseData;
+//            });
+//        }
+
+//        whereScrollPosition = GUILayout.BeginScrollView(whereScrollPosition, false, true);
+
+//        try
+//        {
+//            foreach (var item in whereToDoItems)
+//            {
+//                GUILayout.Label(item.Id + ":" + item.Text);
+//            }
+//        }
+//        catch (Exception e)
+//        {
+//            // Just catches those pesky UI issues
+//        }
+
+//        GUILayout.EndScrollView();
+       
+//        GUILayout.EndArea();
+//    }
+
+
+}
